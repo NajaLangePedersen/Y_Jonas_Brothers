5746cd91e54bb86a4a953b75c024717c49e3a2b4
NAJAS-PC\najap <najapedersen@hotmail.dk>
2026-08-17 09:30:00 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { APITester } from "./APITester";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="app">
+      <div className="logo-container">
+        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
+        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
+      </div>
+
+      <h1>Bun + React</h1>
+      <p>
+        Hi honey!
+      </p>
+      <APITester />
+    </div>
+  );
+}
+
+export default App;
