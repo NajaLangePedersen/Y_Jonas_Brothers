c08944eb48397ff2460bfc74edf848d6a76606c5
Johanna-gesch <johsch01@easv365.dk>
2026-08-17 10:10:23 +0200

    Design change

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,13 @@ export function App() {
 }
 
 function PostComponent(postsItem: PostsItem) {
-    return <div> {postsItem.title}</div>
+    return <div>
+        {postsItem.userId}<br></br>
+        <h1>{postsItem.title}</h1><br></br>
+        {postsItem.body}<br></br><br></br>
+        #{postsItem.tags.join(" #")}<br></br><br></br>
+        views: {postsItem.views}<br></br><br></br><br></br>
+    </div>
 }
 
 export interface Reactions {
